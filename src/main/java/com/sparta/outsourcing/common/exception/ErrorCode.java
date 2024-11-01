583ae6dcab7b93ff4d64fe88374d11a8622cbb52
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-01 18:38:33 +0900

    [#1] feat(ErrorCode ) : 이미 존재하는 email 일 경우 에러 코드 생성

--- src/main/java/com/sparta/outsourcing/common/exception/ErrorCode.java
+++ src/main/java/com/sparta/outsourcing/common/exception/ErrorCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    USER_NOT_FOUND(404, "해당 유저가 없습니다.");
+    USER_NOT_FOUND(404, "해당 유저가 없습니다."),
+    ALREADY_USER_EXIST(400, "존재하는 유저입니다.");
 
     private final int status;
     private final String message;
