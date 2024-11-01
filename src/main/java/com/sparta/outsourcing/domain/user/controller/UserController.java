d4d746325e386a9d09f5b5230c5819b7203b34ae
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-01 18:38:03 +0900

    [#1] feat(UserController ) : 유저 생성 controller 정의

--- src/main/java/com/sparta/outsourcing/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/outsourcing/domain/user/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.sparta.outsourcing.domain.user.controller;
 
+import com.sparta.outsourcing.domain.user.dto.request.UserCreateReqDto;
+import com.sparta.outsourcing.domain.user.dto.response.UserCreateRespDto;
+import com.sparta.outsourcing.domain.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/auth/sign")
+    public ResponseEntity<UserCreateRespDto> join(@RequestBody @Valid UserCreateReqDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(dto));
+    }
+
 }
