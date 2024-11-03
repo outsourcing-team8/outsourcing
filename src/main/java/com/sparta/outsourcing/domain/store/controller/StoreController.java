0778c42733891643c108d0d1b1270009d3760faa
inzulmi <inzulmi132@gmail.com>
2024-11-03 22:51:53 +0900

    [#2] feat(StoreController): validation 적용

--- src/main/java/com/sparta/outsourcing/domain/store/controller/StoreController.java
+++ src/main/java/com/sparta/outsourcing/domain/store/controller/StoreController.java
@@ -4,6 +4,7 @@ import com.sparta.outsourcing.domain.store.dto.request.StoreCreateReqDto;
 import com.sparta.outsourcing.domain.store.dto.response.StoreCreateRespDto;
 import com.sparta.outsourcing.domain.store.service.StoreService;
 import com.sparta.outsourcing.domain.user.entity.User;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,7 @@ public class StoreController {
     private final StoreService storeService;
 
     @PostMapping
-    public ResponseEntity<StoreCreateRespDto> createStore(@AuthenticationPrincipal User user, @RequestBody StoreCreateReqDto reqDto) {
+    public ResponseEntity<StoreCreateRespDto> createStore(@AuthenticationPrincipal User user, @RequestBody @Valid StoreCreateReqDto reqDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(storeService.createStore(user, reqDto));
     }
 }
