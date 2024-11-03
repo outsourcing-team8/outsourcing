311841aff7f78e0392fe8a2d9ada399b8e195da8
jubseok <junseok708@gmail.com>
2024-11-03 12:54:27 +0900

    [#4] fix : 메뉴 생성 Entity 정규식 추가

--- src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuCreateReqDto.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuCreateReqDto.java
@@ -2,14 +2,18 @@ package com.sparta.outsourcing.domain.menu.dto.request;
 
 import com.sparta.outsourcing.domain.menu.entity.Menu;
 import com.sparta.outsourcing.domain.store.entity.Store;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
 public class MenuCreateReqDto {
 
-
+    @NotBlank(message = "메뉴 이릅을 입력해주세요.")
     private String name;
 
+    @NotBlank(message = "메뉴 가격을 입력해주세요.")
+    @Range(min = 0, max = 500000, message = "메뉴 가격은 최대 50만원 까지 가능합니다.")
     private int price;
 
     public Menu toEntity(Store store) {
