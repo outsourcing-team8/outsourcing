8a4b8f7306d4ef146993b529f0def045442f72eb
jubseok <junseok708@gmail.com>
2024-11-03 12:32:49 +0900

    [#4] feat : 메뉴 생성 Dto 생성, req에 builder 적용

--- src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuCreateReqDto.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuCreateReqDto.java
@@ -1,4 +1,22 @@
 package com.sparta.outsourcing.domain.menu.dto.request;
 
+import com.sparta.outsourcing.domain.menu.entity.Menu;
+import com.sparta.outsourcing.domain.store.entity.Store;
+import lombok.Getter;
+
+@Getter
 public class MenuCreateReqDto {
+
+
+    private String name;
+
+    private int price;
+
+    public Menu toEntity(Store store) {
+        return Menu.builder()
+                .store(store)
+                .name(name)
+                .price(price)
+                .build();
+    }
 }
--- src/main/java/com/sparta/outsourcing/domain/menu/dto/response/MenuCreateRespDto.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/dto/response/MenuCreateRespDto.java
@@ -1,5 +1,11 @@
 package com.sparta.outsourcing.domain.menu.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MenuCreateRespDto {
+    private Long id;
 
 }
