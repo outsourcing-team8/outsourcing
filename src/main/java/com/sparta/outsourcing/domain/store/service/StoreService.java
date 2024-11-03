ede3c94a0ba51571db96090ca027f24cb299b455
inzulmi <inzulmi132@gmail.com>
2024-11-03 22:34:51 +0900

    [#2] feat(StoreService): 가게 생성 service 정의

--- src/main/java/com/sparta/outsourcing/domain/store/service/StoreService.java
+++ src/main/java/com/sparta/outsourcing/domain/store/service/StoreService.java
@@ -1,4 +1,28 @@
 package com.sparta.outsourcing.domain.store.service;
 
+import com.sparta.outsourcing.common.exception.CustomApiException;
+import com.sparta.outsourcing.common.exception.ErrorCode;
+import com.sparta.outsourcing.domain.store.dto.request.StoreCreateReqDto;
+import com.sparta.outsourcing.domain.store.dto.response.StoreCreateRespDto;
+import com.sparta.outsourcing.domain.store.entity.Store;
+import com.sparta.outsourcing.domain.store.repository.StoreRepository;
+import com.sparta.outsourcing.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class StoreService {
+
+    private final StoreRepository storeRepository;
+
+    public StoreCreateRespDto createStore(User owner, StoreCreateReqDto reqDto) {
+        if(storeRepository.countByUser(owner) >= 3) {
+            throw new CustomApiException(ErrorCode.TOO_MANY_STORES);
+        }
+        storeRepository.findByName(reqDto.getName())
+                .orElseThrow(() -> new CustomApiException(ErrorCode.ALREADY_STORE_EXIST));
+
+        return new StoreCreateRespDto(storeRepository.save(reqDto.toEntity(owner)));
+    }
 }
