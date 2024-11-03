c62756268d8170416ee99815bdeb9c8374172721
jubseok <junseok708@gmail.com>
2024-11-03 11:11:53 +0900

    [#4] feat : 메뉴 생성 controller 작성

--- src/main/java/com/sparta/outsourcing/domain/menu/controller/MenuController.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/controller/MenuController.java
@@ -1,4 +1,25 @@
 package com.sparta.outsourcing.domain.menu.controller;
 
+import com.sparta.outsourcing.domain.menu.dto.request.MenuCreateReqDto;
+import com.sparta.outsourcing.domain.menu.dto.response.MenuCreateRespDto;
+import com.sparta.outsourcing.domain.menu.service.MenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/stores/{storeId}")
 public class MenuController {
+    private final MenuService menuService;
+
+    @PostMapping("/menu")
+    public ResponseEntity<MenuCreateRespDto> createMenu(
+            @PathVariable Long storeId, @RequestBody MenuCreateReqDto menuCreateReqDto) {
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(menuService.createMenu(storeId,menuCreateReqDto));
+    }
 }
