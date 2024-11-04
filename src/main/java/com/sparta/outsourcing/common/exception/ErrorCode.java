da3e02f3c150a72db9ffd9df53508a974e44d6d4
inzulmi <inzulmi132@gmail.com>
2024-11-04 11:14:43 +0900

    [#8] feat: 가게 정보 수정 기능 구현

--- src/main/java/com/sparta/outsourcing/common/exception/ErrorCode.java
+++ src/main/java/com/sparta/outsourcing/common/exception/ErrorCode.java
@@ -5,14 +5,14 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    STORE_NOT_FOUND(404,"해당 가게가 없습니다."),
-
     USER_NOT_FOUND(404, "해당 유저가 없습니다."),
     ALREADY_USER_EXIST(400, "존재하는 유저입니다."),
     MISS_MATCH_PASSWORD(400, "패스워드 검증에 실패하였습니다."),
 
     TOO_MANY_STORES(409, "이미 3개의 가게를 가지고 있습니다."),
     ALREADY_STORE_EXIST(409, "같은 상호명의 가게가 이미 존재합니다."),
+    STORE_NOT_FOUND(404, "해당 가게가 없습니다."),
+    NOT_STORE_OWNER(401, "해당 가게의 사장이 아닙니다."),
     ;
 
     private final int status;
--- src/main/java/com/sparta/outsourcing/domain/store/service/StoreService.java
+++ src/main/java/com/sparta/outsourcing/domain/store/service/StoreService.java
@@ -3,11 +3,17 @@ package com.sparta.outsourcing.domain.store.service;
 import com.sparta.outsourcing.common.exception.CustomApiException;
 import com.sparta.outsourcing.common.exception.ErrorCode;
 import com.sparta.outsourcing.domain.store.dto.request.StoreCreateReqDto;
+import com.sparta.outsourcing.domain.store.dto.request.StoreUpdateReqDto;
 import com.sparta.outsourcing.domain.store.dto.response.StoreCreateRespDto;
+import com.sparta.outsourcing.domain.store.dto.response.StoreUpdateRespDto;
+import com.sparta.outsourcing.domain.store.entity.Store;
 import com.sparta.outsourcing.domain.store.repository.StoreRepository;
 import com.sparta.outsourcing.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +29,17 @@ public class StoreService {
                 .ifPresent(store -> {throw new CustomApiException(ErrorCode.ALREADY_STORE_EXIST);});
 
         return new StoreCreateRespDto(storeRepository.save(reqDto.toEntity(owner)));
+    }
+
+    @Transactional
+    public StoreUpdateRespDto updateStore(User user, Long storeId, StoreUpdateReqDto reqDto) {
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new CustomApiException(ErrorCode.STORE_NOT_FOUND));
+        if(!Objects.equals(user.getUserId(), store.getOwner().getUserId())) {
+            throw new CustomApiException(ErrorCode.NOT_STORE_OWNER);
+        }
+
+        store.update(reqDto.getOpenAt(), reqDto.getClosedAt(), reqDto.getMinPrice());
+        return new StoreUpdateRespDto(storeRepository.saveAndFlush(store));
     }
 }
