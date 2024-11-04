b8cd7938697c59d386e5665f612f82674a8ff2f7
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-04 16:13:43 +0900

    [#32] feat(ErrorCode) : 에러 코드 추가

--- src/main/java/com/sparta/outsourcing/common/exception/ErrorCode.java
+++ src/main/java/com/sparta/outsourcing/common/exception/ErrorCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    STORE_NOT_FOUND(404,"해당 가게가 없습니다."),
+    STORE_NOT_FOUND(404, "해당 가게가 없습니다."),
 
     USER_NOT_FOUND(404, "해당 유저가 없습니다."),
     ALREADY_USER_EXIST(400, "존재하는 유저입니다."),
@@ -13,6 +13,9 @@ public enum ErrorCode {
 
     TOO_MANY_STORES(409, "이미 3개의 가게를 가지고 있습니다."),
     ALREADY_STORE_EXIST(409, "같은 상호명의 가게가 이미 존재합니다."),
+    INVALID_TOKEN_ERROR(401, "잘못된 토큰 정보입니다."),
+    TOKEN_NOT_FOUND(401, "토큰 정보를 찾을 수 없습니다."),
+    AUTHORITY_NOT_FOUND(403, "권한 정보를 찾을 수 없습니다."),
     ;
 
     private final int status;
