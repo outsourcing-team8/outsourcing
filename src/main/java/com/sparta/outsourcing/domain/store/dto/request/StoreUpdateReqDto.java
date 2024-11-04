af89dfa233222d1901c71462d8c02d0d8db64814
inzulmi <inzulmi132@gmail.com>
2024-11-04 18:51:10 +0900

    [#8] fix: not blink validation 추가

--- src/main/java/com/sparta/outsourcing/domain/store/dto/request/StoreUpdateReqDto.java
+++ src/main/java/com/sparta/outsourcing/domain/store/dto/request/StoreUpdateReqDto.java
@@ -1,5 +1,6 @@
 package com.sparta.outsourcing.domain.store.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -9,12 +10,15 @@ import java.time.LocalTime;
 @Getter
 public class StoreUpdateReqDto {
 
+    @NotBlank(message = "영업 시작 시간을 입력해주세요.")
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime openAt;
 
+    @NotBlank(message = "영업 마감 시간을 입력해주세요.")
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime closedAt;
 
+    @NotBlank(message = "최소 주문 가격을 입력해주세요.")
     @Range(min = 100, max = 100_000_000, message = "최소 100원부터 최대 1억원 까지만 입력해주세요.")
     private Integer minPrice;
 }
