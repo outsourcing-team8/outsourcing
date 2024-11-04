d8cdfdbee298e627bb370627607629d5e9d7e85b
jubseok <junseok708@gmail.com>
2024-11-04 12:50:43 +0900

    [#11] feat : 메뉴 수정 Service 생성

--- src/main/java/com/sparta/outsourcing/domain/menu/service/MenuService.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/service/MenuService.java
@@ -3,12 +3,18 @@ package com.sparta.outsourcing.domain.menu.service;
 import com.sparta.outsourcing.common.exception.CustomApiException;
 import com.sparta.outsourcing.common.exception.ErrorCode;
 import com.sparta.outsourcing.domain.menu.dto.request.MenuCreateReqDto;
+import com.sparta.outsourcing.domain.menu.dto.request.MenuPatchReqDto;
 import com.sparta.outsourcing.domain.menu.dto.response.MenuCreateRespDto;
+import com.sparta.outsourcing.domain.menu.dto.response.MenuPatchRespDto;
+import com.sparta.outsourcing.domain.menu.entity.Menu;
 import com.sparta.outsourcing.domain.menu.repository.MenuRepository;
 import com.sparta.outsourcing.domain.store.entity.Store;
 import com.sparta.outsourcing.domain.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +28,22 @@ public class MenuService {
                 -> new CustomApiException(ErrorCode.STORE_NOT_FOUND));
         return new MenuCreateRespDto(menuRepository.save(dto.toEntity(store)).getMenuId());
     }
+
+    @Transactional
+    public MenuPatchRespDto patchMenu(
+            Long storeId, Long menuId, MenuPatchReqDto dto) {
+        Store store = storeRepository.findById(storeId).orElseThrow(()
+                -> new CustomApiException(ErrorCode.STORE_NOT_FOUND));
+
+        Menu menu = menuRepository.findById(menuId).orElseThrow(()
+                -> new CustomApiException(ErrorCode.MENU_NOT_FOUND));
+
+        if (!Objects.equals(store.getStoreId(), menu.getStore().getStoreId())) {
+            throw new CustomApiException(ErrorCode.STORE_NOT_FOUND);
+        }
+        dto.toEntity(store);
+        return dto.toDto(menuId,dto);
+    }
+
 
 }
