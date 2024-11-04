bd14b05b886b5edc975591ed39c5062a700d8471
inzulmi <inzulmi132@gmail.com>
2024-11-04 15:30:25 +0900

    [#33] fix: 오차 수정 및 ErrorCode 추가

--- src/main/java/com/sparta/outsourcing/common/exception/ErrorCode.java
+++ src/main/java/com/sparta/outsourcing/common/exception/ErrorCode.java
@@ -13,7 +13,9 @@ public enum ErrorCode {
     TOO_MANY_STORES(409, "이미 3개의 가게를 가지고 있습니다."),
     ALREADY_STORE_EXIST(409, "같은 상호명의 가게가 이미 존재합니다."),
 
-    ALREADY_REIVEWED_ORDER(409, "이미 리뷰한 주문입니다."),
+    ORDER_NOT_FOUND(404, "해당 주문이 없습니다."),
+    NOT_ORDERED(401, "주문자만 리뷰를 작성할 수 있습니다."),
+    ALREADY_REVIEWED_ORDER(409, "이미 리뷰한 주문입니다."),
     ;
 
     private final int status;
--- src/main/java/com/sparta/outsourcing/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/outsourcing/domain/review/service/ReviewService.java
@@ -6,7 +6,6 @@ import com.sparta.outsourcing.domain.order.entity.Order;
 import com.sparta.outsourcing.domain.order.repository.OrderRepository;
 import com.sparta.outsourcing.domain.review.dto.request.ReviewCreateReqDto;
 import com.sparta.outsourcing.domain.review.dto.response.ReviewCreateRespDto;
-import com.sparta.outsourcing.domain.review.entity.Review;
 import com.sparta.outsourcing.domain.review.repository.ReviewRepository;
 import com.sparta.outsourcing.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -25,10 +24,10 @@ public class ReviewService {
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new CustomApiException(ErrorCode.ORDER_NOT_FOUND));
         if(!Objects.equals(user.getUserId(), order.getUser().getUserId())) {
-            throw new CustomApiException(ErrorCode.NOT_ORDERER_USER);
+            throw new CustomApiException(ErrorCode.NOT_ORDERED);
         }
         reviewRepository.findByOrder(order)
-                .ifPresent(review -> {throw new CustomApiException(ErrorCode.ALREADY_REIVEWED_ORDER);});
+                .ifPresent(review -> {throw new CustomApiException(ErrorCode.ALREADY_REVIEWED_ORDER);});
 
         return new ReviewCreateRespDto(reviewRepository.save(reqDto.toEntity(order)));
     }
