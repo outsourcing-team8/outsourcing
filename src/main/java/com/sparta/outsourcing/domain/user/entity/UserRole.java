3e4c1af2206c33c4b6705a106fe00a73533e0121
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-04 14:38:43 +0900

    [#22] refactor(UserRole) : 권한 정보 수정

--- src/main/java/com/sparta/outsourcing/domain/user/entity/UserRole.java
+++ src/main/java/com/sparta/outsourcing/domain/user/entity/UserRole.java
@@ -1,6 +1,15 @@
 package com.sparta.outsourcing.domain.user.entity;
 
-public enum UserRole {
-    USER, OWNER
+import lombok.Getter;
 
+@Getter
+public enum UserRole {
+    USER("ROLE_USER"),
+    OWNER("ROLE_OWNER");
+
+    private String authority;
+
+    UserRole(String authority) {
+        this.authority = authority;
+    }
 }
