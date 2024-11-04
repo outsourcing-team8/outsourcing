e5ab64f2e679a94d850eab23a98430058d317408
jubseok <junseok708@gmail.com>
2024-11-04 14:05:01 +0900

    [#23] feat : 메뉴 조회 Controller 생성

--- src/main/java/com/sparta/outsourcing/domain/menu/controller/MenuController.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/controller/MenuController.java
@@ -3,13 +3,18 @@ package com.sparta.outsourcing.domain.menu.controller;
 import com.sparta.outsourcing.domain.menu.dto.request.MenuCreateReqDto;
 import com.sparta.outsourcing.domain.menu.dto.request.MenuPatchReqDto;
 import com.sparta.outsourcing.domain.menu.dto.response.MenuCreateRespDto;
+import com.sparta.outsourcing.domain.menu.dto.response.MenuGetRespDto;
 import com.sparta.outsourcing.domain.menu.dto.response.MenuPatchRespDto;
 import com.sparta.outsourcing.domain.menu.service.MenuService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,17 +29,25 @@ public class MenuController {
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(menuService.createMenu(storeId,dto));
+                .body(menuService.createMenu(storeId, dto));
     }
 
     @PatchMapping("/menu/{menuId}")
     public ResponseEntity<MenuPatchRespDto> patchMenu(
             @PathVariable Long storeId,
             @PathVariable Long menuId,
-            @RequestBody @Valid MenuPatchReqDto dto){
+            @RequestBody @Valid MenuPatchReqDto dto) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(menuService.patchMenu(storeId,menuId,dto));
+                .body(menuService.patchMenu(storeId, menuId, dto));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<MenuGetRespDto>> getMenuList(
+            @PathVariable Long storeId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(menuService.getMuenuList());
     }
 
 }
