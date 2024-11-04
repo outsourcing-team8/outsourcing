88838566d74cc53903305385a5d48644461655ce
jubseok <junseok708@gmail.com>
2024-11-04 17:16:56 +0900

    [#40] feat : 메뉴 삭제 controller 생성

--- src/main/java/com/sparta/outsourcing/domain/menu/controller/MenuController.java
+++ src/main/java/com/sparta/outsourcing/domain/menu/controller/MenuController.java
@@ -3,6 +3,7 @@ package com.sparta.outsourcing.domain.menu.controller;
 import com.sparta.outsourcing.domain.menu.dto.request.MenuCreateReqDto;
 import com.sparta.outsourcing.domain.menu.dto.request.MenuPatchReqDto;
 import com.sparta.outsourcing.domain.menu.dto.response.MenuCreateRespDto;
+import com.sparta.outsourcing.domain.menu.dto.response.MenuDeleteRespDto;
 import com.sparta.outsourcing.domain.menu.dto.response.MenuGetRespDto;
 import com.sparta.outsourcing.domain.menu.dto.response.MenuPatchRespDto;
 import com.sparta.outsourcing.domain.menu.service.MenuService;
@@ -56,7 +57,17 @@ public class MenuController {
             @PathVariable Long storeId) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(menuService.getMuenu(menuId, storeId));
+                .body(menuService.getMenu(menuId, storeId));
+    }
+
+    @DeleteMapping("{menuId}")
+    public ResponseEntity<MenuDeleteRespDto> deleteMenu(
+            @PathVariable Long menuId,
+            @PathVariable Long storeId) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(menuService.deleteMenu(menuId, storeId));
+
     }
 
 }
