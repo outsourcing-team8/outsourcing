9df3e7953668d5ac171e84b1eb8cbab18f858648
inzulmi <inzulmi132@gmail.com>
2024-11-04 11:45:45 +0900

    [#13] feat: 가게 다건 조회 기능 구현

--- src/main/java/com/sparta/outsourcing/domain/store/controller/StoreController.java
+++ src/main/java/com/sparta/outsourcing/domain/store/controller/StoreController.java
@@ -2,17 +2,16 @@ package com.sparta.outsourcing.domain.store.controller;
 
 import com.sparta.outsourcing.domain.store.dto.request.StoreCreateReqDto;
 import com.sparta.outsourcing.domain.store.dto.response.StoreCreateRespDto;
+import com.sparta.outsourcing.domain.store.dto.response.StoreGetRespDto;
 import com.sparta.outsourcing.domain.store.service.StoreService;
 import com.sparta.outsourcing.domain.user.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +23,15 @@ public class StoreController {
     @PostMapping
     public ResponseEntity<StoreCreateRespDto> createStore(@AuthenticationPrincipal User user, @RequestBody @Valid StoreCreateReqDto reqDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(storeService.createStore(user, reqDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<StoreGetRespDto>> getStores(
+            @RequestParam(name = "page") int page,
+            @RequestParam(name = "size", required = false, defaultValue = "10") int size,
+            @RequestParam(name = "name", required = false) String name) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(storeService.getStores(page-1, size, name));
     }
 }
--- src/main/java/com/sparta/outsourcing/domain/store/service/StoreService.java
+++ src/main/java/com/sparta/outsourcing/domain/store/service/StoreService.java
@@ -4,9 +4,13 @@ import com.sparta.outsourcing.common.exception.CustomApiException;
 import com.sparta.outsourcing.common.exception.ErrorCode;
 import com.sparta.outsourcing.domain.store.dto.request.StoreCreateReqDto;
 import com.sparta.outsourcing.domain.store.dto.response.StoreCreateRespDto;
+import com.sparta.outsourcing.domain.store.dto.response.StoreGetRespDto;
 import com.sparta.outsourcing.domain.store.repository.StoreRepository;
 import com.sparta.outsourcing.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,5 +27,11 @@ public class StoreService {
                 .ifPresent(store -> {throw new CustomApiException(ErrorCode.ALREADY_STORE_EXIST);});
 
         return new StoreCreateRespDto(storeRepository.save(reqDto.toEntity(owner)));
+    }
+
+    public Page<StoreGetRespDto> getStores(int page, int size, String name) {
+        Pageable pageable = PageRequest.of(page, size);
+        if(name == null ||  name.isEmpty()) return storeRepository.findAllByDeletedIsFalse(pageable).map(StoreGetRespDto::new);
+        return storeRepository.findAllByDeletedIsFalseAndNameContaining(name, pageable).map(StoreGetRespDto::new);
     }
 }
