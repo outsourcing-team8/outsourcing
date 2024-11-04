c4b62946112438d05b5907388cd0634ed04747a8
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-04 13:09:47 +0900

    [#22] feat(UserController) : 유저 컨틀롤러 구현

--- src/main/java/com/sparta/outsourcing/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/outsourcing/domain/user/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.sparta.outsourcing.domain.user.controller;
 
 import com.sparta.outsourcing.domain.user.dto.request.UserCreateReqDto;
+import com.sparta.outsourcing.domain.user.dto.request.UserDeleteReqDto;
 import com.sparta.outsourcing.domain.user.dto.response.UserCreateRespDto;
+import com.sparta.outsourcing.domain.user.dto.response.UserDeleteRespDto;
 import com.sparta.outsourcing.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +26,12 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(dto));
     }
 
+    @DeleteMapping("/auth/users")
+    public ResponseEntity<UserDeleteRespDto> deleteCurrentUser(
+            @AuthenticationPrincipal LogignUser logignUser,
+            @RequestBody @Valid UserDeleteReqDto reqDto
+    ) {
+        UserDeleteRespDto respDto = userService.deleteUser(loginUser.getUserId(), reqDto);
+        return ResponseEntity.status(HttpStatus.OK).body(respDto);
+    }
 }
