ecb6c6797672aeddd6b52ca004a716a2324448d0
jubseok <junseok708@gmail.com>
2024-11-04 11:46:29 +0900

    [#11] feat : 메뉴 수정 Dto 생성

---
+++ src/main/java/com/sparta/outsourcing/domain/menu/dto/request/MenuPatchReqDto.java
@@ -0,0 +1,28 @@
+package com.sparta.outsourcing.domain.menu.dto.request;
+
+import com.sparta.outsourcing.domain.menu.entity.Menu;
+import com.sparta.outsourcing.domain.store.entity.Store;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+@Getter
+@NoArgsConstructor
+public class MenuPatchReqDto {
+
+    @NotBlank(message = "메뉴 이름을 입력해주세요.")
+    private String name;
+
+    @NotBlank(message = "메뉴 가격을 입력해주세요.")
+    @Range(min = 0, max = 500000, message = "메뉴 가격은 최대 50만원 까지 가능합니다.")
+    private int price;
+
+    public Menu toEntity(Store store) {
+        return Menu.builder()
+                .store(store)
+                .name(name)
+                .price(price)
+                .build();
+    }
+}
