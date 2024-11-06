910ba47775647fca2f91c6520a598a4254705ed3
hemgom <reborn.april23@gmail.com>
2024-11-06 16:03:42 +0900

    [#72] feat: 테스트를 위해 OrderAddReqDto 에 @Builder 추가

--- src/main/java/com/sparta/outsourcing/domain/order/dto/request/OrderAddReqDto.java
+++ src/main/java/com/sparta/outsourcing/domain/order/dto/request/OrderAddReqDto.java
@@ -2,9 +2,11 @@ package com.sparta.outsourcing.domain.order.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class OrderAddReqDto {
 	@NotNull
 	private Long menuId;
