5cd23cf1233ce8e5de812014916edd295013f7b4
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-06 19:17:51 +0900

    [#81] feat(SecurityConfig) : login 성공시 controller 구현

--- src/main/java/com/sparta/outsourcing/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/outsourcing/domain/user/controller/UserController.java
@@ -1,20 +1,19 @@
 package com.sparta.outsourcing.domain.user.controller;
 
+import com.sparta.outsourcing.common.oauth2.OAuthLoginRespDto;
 import com.sparta.outsourcing.common.security.LoginUser;
 import com.sparta.outsourcing.domain.user.dto.request.UserCreateReqDto;
 import com.sparta.outsourcing.domain.user.dto.request.UserDeleteReqDto;
 import com.sparta.outsourcing.domain.user.dto.response.UserCreateRespDto;
 import com.sparta.outsourcing.domain.user.dto.response.UserDeleteRespDto;
 import com.sparta.outsourcing.domain.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,6 +32,18 @@ public class UserController {
             @RequestBody @Valid UserDeleteReqDto reqDto
     ) {
         UserDeleteRespDto respDto = userService.deleteUser(loginUser.getUser().getUserId(), reqDto);
+        return ResponseEntity.status(HttpStatus.OK).body(respDto);
+    }
+
+    @GetMapping("/login/success")
+    public ResponseEntity<OAuthLoginRespDto> oauthLoginSuccess(
+            @AuthenticationPrincipal LoginUser loginUser, HttpServletRequest request
+    ) {
+        OAuthLoginRespDto respDto = new OAuthLoginRespDto(
+                loginUser.getUser().getUserId(),
+                loginUser.getUsername(),
+                request.getHeader("Authorization"));
+
         return ResponseEntity.status(HttpStatus.OK).body(respDto);
     }
 }
