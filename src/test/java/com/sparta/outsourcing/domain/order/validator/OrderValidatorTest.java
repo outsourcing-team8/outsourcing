fa763c74129bc654997b923512821b0b5bcc27bb
hemgom <reborn.april23@gmail.com>
2024-11-06 18:03:39 +0900

    [#72] feat: 가계 영업시간 외에 주문 예외 테스트 작성 및 오류 발견

--- src/test/java/com/sparta/outsourcing/domain/order/validator/OrderValidatorTest.java
+++ src/test/java/com/sparta/outsourcing/domain/order/validator/OrderValidatorTest.java
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +28,32 @@ class OrderValidatorTest {
         // when & then
         assertThrows(CustomApiException.class, () ->
             orderValidator.checkMinPrice(storeMinPrice, menuPrice, amount)
+        );
+    }
+
+    @Test
+    @DisplayName("가게영업시간(19시~21시) 외에 주문하는 경우")
+    void checkBusinessHoursStoreNotOpen1() {
+        // given
+        LocalTime storeOpenAt = LocalTime.of(19, 0);
+        LocalTime storeClosedAt = LocalTime.of(21, 0);
+
+        // when & then
+        assertThrows(CustomApiException.class, () ->
+                orderValidator.checkBusinessHours(storeOpenAt, storeClosedAt)
+        );
+    }
+
+    @Test
+    @DisplayName("가게영업시간(18시~06시) 외에 주문하는 경우")
+    void checkBusinessHoursStoreNotOpen2() {
+        // given
+        LocalTime storeOpenAt = LocalTime.of(18, 0);
+        LocalTime storeClosedAt = LocalTime.of(6, 0);
+
+        // when & then
+        assertThrows(CustomApiException.class, () ->
+                orderValidator.checkBusinessHours(storeOpenAt, storeClosedAt)
         );
     }
 }
